45f4f8d1bc6b68aa2bc75703f6bc48b2393a6352
Adinath-R-Dhole <adinathdhole149@gmail.com>
2026-05-01 19:20:57 +0530

    initial project setup

---
+++ src/test/java/com/Blogging_Platform/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Blogging_Platform.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
